1e4b42e1678bef7b42905e4ac09f88144926734c
George Trutnev <tghost@mail.ru>
2022-11-15 10:48:03 +0300

    .

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -en
+
+# сборка приложения 
+npm run build
+
+# переход в каталог сборки 
+cd dist
+
+#Инициализация репозитория и загрузка кода github
+git init
+git add -A 
+git commit -m 'deploy'
+
+$ git push -f https://github.com/tghostGosha/vue-app.git master:gh-pages
+
+cd -
